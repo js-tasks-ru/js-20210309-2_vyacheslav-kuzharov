29621ef58026b0ba8b806a412a33bae4c4e65643
Vyacheslav Kuzharov <vyacheslav.kuzharov@gmail.com>
2021-03-13 19:48:26 +0300

    sortStrings function implementation

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,17 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const validDirections = ['asc', 'desc'];
+  const sortedAry = [...arr];
 
+  if (!validDirections.includes(param)) {
+    console.log(`Invalid sort param: ${param}`);
+    return arr;
+  }
+
+  const direction = (param === 'asc') ? 1 : -1;
+
+  sortedAry.sort((a, b) => direction * a.localeCompare(b, ["ru", "en"], {caseFirst: 'upper'}));
+
+  return sortedAry;
 }
