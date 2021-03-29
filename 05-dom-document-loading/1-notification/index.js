c94dc247844846596a93ec174c85249e326168b3
Vyacheslav Kuzharov <vyacheslav.kuzharov@gmail.com>
2021-03-29 21:07:21 +0300

    Added NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+  constructor(message = '', {duration = 0, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:20s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = this.template;
+
+    this.element = rootElement.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.currentMessage) {NotificationMessage.currentMessage.remove();}
+
+    NotificationMessage.currentMessage = this.element;
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  destroy() {
+    if (this.element) {
+      this.remove();
+      this.element = null;
+    }
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
